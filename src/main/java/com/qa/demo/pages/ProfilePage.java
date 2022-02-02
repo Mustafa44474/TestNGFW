643048ef8b932ed0d54c2dd7f4df59e754c9decc
Mustafa44474 <gmustafa.siddiqi@gmail.com>
2022-02-01 22:25:01 -0800

    profilePageUpdate Code

---
+++ src/main/java/com/qa/demo/pages/ProfilePage.java
@@ -0,0 +1,12 @@
+package com.qa.demo.pages;
+
+public class ProfilePage {
+	public ProfilePage() {
+		System.out.println("PP -Default Constructor...........");
+	
+	}
+	public void getTtile() {
+		System.out.println("PP--- Title");
+	}
+
+}
